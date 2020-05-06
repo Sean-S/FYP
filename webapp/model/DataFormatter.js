f8c25749ca918e86593e40bc08ce48a9434354e0
Sean-S <sean.sexton@mycit.ie>
2020-05-06 11:13:07 +0100

    Add files via upload

---
+++ webapp/model/DataFormatter.js
@@ -0,0 +1,10 @@
+sap.ui.define(["sap/ui/base/Object"],
+	function(Object) {
+		return Object.extend("sap.ui.demo.bulletinboard.model.DateFormatter", {
+			format: function() {
+				return "";
+
+			}
+		});
+	}
+);
